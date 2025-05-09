8a8c99a8f2a6586e638584e5c542474c750552a7
NONAME-2121237 <2710949520@qq.com>
2025-05-09 21:52:45 +0800

    闲的

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("你好，{}！这是来自Rust的问候！", name)  // 我好无聊，把这玩意都改成中文了.....
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
